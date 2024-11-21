bb456c29d7d68f94d91d5b129116e0c316c03066
TechnoSlash <Riwajcarkey@gmail.com>
2024-11-21 15:19:18 +0545

    NM lab 1 final

--- NM/bisection_method.cpp
+++ NM/bisection_method.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
-#include <cmath>
+#include <cmath> // for pow and fabs function.
+#include <cstdlib> // for exit function.
 using namespace std;
-#define f(x) pow(x, 3) - 4 * x - 9
+#define f(x) pow(x, 3.0) - 4.0 * x - 9.0 // defining the function f(x)
 int main() {
-  float a, b , e, steps = 0;
+  float a, b, e;
   cout << "Enter two inital guesses." << endl;
   cin >> a >> b;
   cout << "Enter tolerable error." << endl;
   cin >> e;
 
-  if (f(a) * f(b) > 0) {
+  if (f(a) * f(b) > 0) { // f(a) and f(b) must have alternate sign.
     cout << "Your two inital guesses has same sign of their corresponding function. " << endl;
-    exit(1);
+    exit(1); //Terminating the program with the error 1.
   }
-  if(f(a) > f(b)) {
-    float temp;
+  if(f(a) > f(b)) { // Making sure that f(a) is -ve and f(b) is +ve as in line 29, b is replaced
+    float temp;     // by c when f(c) is +ve and vice versa.
     temp = a;
     a = b;
     b = temp;
   }
-
-  float c;
+  float c, steps = 0;
   cout << "\t\t" << "steps" << "\t" << "a" << "\t" << "b" << "\t" << "c" << "\t" << "f(c)" << endl;
-  do { // Here fabs is the absolute value function
+  do { 
     c = (a + b) / 2;
     steps++;
     cout << "\t\t" << steps << "\t" << a << "\t" << b << "\t" << c << "\t" << f(c) << endl;
@@ -31,7 +31,7 @@ int main() {
     } else if ( f(c) < 0) {
       a = c;
     }
-  } while(fabs(f(c)) > e);
+  } while(fabs(f(c)) > e); // Here, fabs is the absolute value function
   cout << "\t" <<  "Result = " << c << endl;
   return 0;
 }
@@ -49,3 +49,20 @@ Enter tolerable error.
                 7       2.70312 2.71875 2.71094 0.0794234
                 8       2.70312 2.71094 2.70703 0.00904924
         Result = 2.70703 */
+
+        // Start
+        // Define function f(x)
+        // Enter two initial guesses and error (say a, b and e)
+        // if (f(a) * f(b) > 0)
+        //   print "f(a) and f(b) must have alternate sign." and exit
+        // if (f(a) > f(b))
+        //   exchange their values to make sure f(a) is -ve and f(b) is +ve
+        // do {
+        //   c = (a + b) * 0.5
+        //   if( f(c) > 0) {
+        //     b = c
+        //   } else if ( f(c) < 0) {
+        //     a = c
+        // } while (fabs(f(c)) > e)
+        // Print c
+        // Exit
